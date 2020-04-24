57cd77c4f1fcf617bb9eb4af8f6199081b1ad7df
祝旭 <1150559257@qq.com>
2020-04-24 22:52:23 +0800

    更新学习

---
+++ Code/test 4_24_1/test 4_24_1/test.c
@@ -0,0 +1,80 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+//struct Stu 
+//{
+//	int i;
+//	char j;
+//	char h;
+////8
+//};
+//struct Stu
+//{
+//	char j;
+//	int i;
+//	char h;
+//	//12
+//};
+//struct Stu
+//{
+//	char i;
+//	double j;
+//	int h;
+//	//24
+//};
+//struct Stu
+//{
+//	double j;
+//	char i;
+//	int h;
+//	//16
+//};
+//int main()
+//{
+//	printf("%d ", sizeof(struct Stu));
+//	return 0;
+//}
+//struct Stu1
+//{
+//	double i;
+//	char j;
+//	int h;
+//};
+//struct Stu2
+//{
+//	int n;
+//	struct Stu1 m;
+//	char q;
+//};
+//int main()
+//{
+//	printf("%d ", sizeof(struct Stu2));
+//	return 0;
+//}
+//struct Stu
+//{
+//	double i;
+//	char j;
+//	int k;
+//	//16
+//};
+//struct Stu
+//{
+//	char i;
+//	int j;
+//	double k;
+//	//16
+//}
+//#pragma pack(8)
+//struct Stu
+//{
+//	char i;
+//	double k;
+//	int j;
+//	//24
+//};
+//int main()
+//{
+//	printf("%d ", sizeof(struct Stu));
+//	return 0;
+//}
