45a7d1ac0e882713b67563ffe761ca43be31fa4f
祝旭 <1150559257@qq.com>
2020-05-16 22:26:33 +0800

    更新学习

---
+++ Code/test 5_15/test 5_15/test.c
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main()
+//{
+//	int n = 0;
+//	int m = 0;
+//	int sum = 0;
+//	int i = 0;
+//	int j = 0;
+//	int d[100] = { 0 };
+//	int tmp = 0;
+//	scanf("%d", &n);
+//	for (m = 0; m<n; m++)
+//	{
+//		scanf("%d", &d[m]);
+//	}
+//	for (i = 0; i<n; i++)
+//	{
+//		for (j = 0; j < n - i; j++)
+//		{
+//			if (d[j] < d[j + 1])
+//			{
+//				tmp = d[j];
+//				d[j] = d[j + 1];
+//				d[j + 1] = tmp;
+//			}
+//		}
+//	}
+//	sum = d[0] * d[1] * d[2];
+//	printf("%d\n", sum);
+//	return 0;
+//}
+unsigned char i = 0;
+int main()
+{
+	for (i = 0; i <= 255; i++)
+	{
+		printf("hello world\n");
+	}
+	return 0;
+}
