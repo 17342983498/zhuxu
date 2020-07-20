c2d8908238e5f77cdbcbdad2c7d98aaaf6ea4944
祝旭 <1150559257@qq.com>
2020-07-20 18:10:53 +0800

    快速排序（挖坑法）

---
+++ Code/test 7-20-1/test 7-20-1/QuickSort.h
@@ -0,0 +1,7 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+void swap(int* left, int*right);
+int Partion(int array[], int left, int right);
+void QuickSort(int array[], int left, int right);
+void Print(int array[], int size);
+void TestSort();
---
+++ Code/test 7-20-1/test 7-20-1/test.c
@@ -0,0 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include"QuickSort.h"
+int main()
+{
+	TestSort();
+	return 0;
+}
