1c1f8f9b0a15b8d8af22bc8c9633f67314b55c67
祝旭 <1150559257@qq.com>
2020-07-20 18:10:06 +0800

    快速排序

---
+++ Code/test 7-20/test 7-20/QuickSort.c
@@ -0,0 +1,91 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include"Sort.h"
+void TestSort()
+{
+	int array[] = { 4, 2, 6, 8, 7, 1, 0, 5, 9, 3 };
+	int size = sizeof(array) / sizeof(array[0]);
+	Print(array, sizeof(array) / sizeof(array[0]));
+	QuickSort(array, 0, size);
+	Print(array, sizeof(array) / sizeof(array[0]));
+
+}
+
+void Print(int array[], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		printf("%d ", array[i]);
+	}
+	printf("\n");
+}
+
+void QuickSort(int array[], int left,int right)
+{
+	
+	if (right - left>1)
+	{
+		int div = Partion(array, left, right);
+		QuickSort(array, left, div);
+		QuickSort(array, div + 1, right);
+	}
+}
+//int Partion(int array[], int left, int right)
+//{
+//	int begin = left;
+//	int end = right-1;
+//	int key = array[right-1];
+//	while (begin < end)
+//	{
+//		while (array[begin] <= key && begin < end)
+//		{
+//			begin++;
+//		}
+//		while (array[end]>=key && begin < end)
+//		{
+//			end--;
+//		}
+//	/*	if (begin<end)*/
+//		swap(&array[begin], &array[end]);
+//	}
+//	swap(&array[begin], &array[right-1]);
+//	return begin;
+//}
+
+//ÍÚ¿Ó·¨
+int Partion(int array[], int left, int right)
+{
+	int begin = left;
+	int end = right - 1;
+	int key = array[right - 1];
+	while (begin < end)
+	{
+		while (array[begin] <= key && begin < end)
+		{
+			begin++;
+		}
+		if (begin < end)
+		{
+			array[end] = array[begin];
+			end--;
+		}
+		while (array[end] >= key && begin < end)
+		{
+			end--;
+		}
+		if (begin < end)
+		{
+			array[begin] = array[end];
+		}
+	}
+	array[begin] = key;
+	return begin;
+}
+
+void swap(int* left, int*right)
+{
+	int temp = *left;
+	*left = *right;
+	*right = temp;
+}
---
+++ Code/test 7-20/test 7-20/test.c
@@ -0,0 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include"Sort.h"
+int main()
+{
+	TestSort();
+	return 0;
+}
