fb700991f5ce29e266235d722d6c380d8b05e6a9
祝旭 <1150559257@qq.com>
2020-08-26 21:51:40 +0800

    赋值运算符重载

---
+++ Code/C++ 8-26/C++ 8-26/test.cpp
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<iostream>
+using namespace std;
+
+class Date
+{
+public:
+	Date(int year = 2000, int month = 1, int day = 9)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+	Date(const Date& a)
+	{
+		_year = a._year;
+		_month = a._month;
+		_day = a._day;
+		cout << _year << " " << _month << " " << _day << endl;
+	}
+	Date& operator=(const Date& a)
+	{
+		if (this != &a)
+		{
+			_year = a._year;
+			_month = a._month;
+			_day = a._day;
+			cout << _year << " " << _month << " " << _day << endl;
+		}
+		return *this;
+	}
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+int main()
+{
+	Date a1(2020, 9, 26);
+	Date a2(a1);
+	a2 = a1;
+	return 0;
+}
