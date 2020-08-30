8714ca1cfe23dc9e53a41cf0737fd2e410004022
祝旭 <1150559257@qq.com>
2020-08-30 22:27:29 +0800

    更新学习

---
+++ Code/test 8-30/test 8-30/test.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+int main() 
+{ 
+	int a = 5; 
+	if (a = 0)
+	{ 
+		printf("%d", a - 10); 
+	} 
+	else 
+	{ 
+		printf("%d", a++); 
+	}
+	return 0; 
+}
