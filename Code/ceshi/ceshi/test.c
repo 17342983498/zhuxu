0f909b3380b87a700815ebf34a570ea8f9dc65b8
祝旭 <1150559257@qq.com>
2022-10-05 18:03:42 +0800

    牛客网刷题

---
+++ Code/ceshi/ceshi/test.c
@@ -0,0 +1,66 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//int Add(int a, int b)
+//{
+//	int x = 0;
+//	x = a + b;
+//	return x;
+//}
+//
+//
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int ret = Add(a, b);
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+//void print(int n)
+//{
+//	if (n > 0)
+//	{
+//		int x = n / 6;
+//		print(x);
+//		printf("%d", (n % 6));
+//	}
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	print(n);
+//	return 0;
+//}
+
+//#include <stdlib.h>
+//void print(int n)
+//{
+//	if (n > 0)
+//	{
+//		int x = n / 16;
+//		print(x);
+//		int num = n % 16;
+//		if (num<16&&num>9)
+//		{
+//			printf("%c", (num - 10 + 'A'));
+//		}
+//		else
+//		{
+//			printf("%d", num);
+//		}
+//			
+//	}
+//}
+//
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	print(n);
+//	return 0;
+//}
