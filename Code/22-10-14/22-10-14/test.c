0c088fe2c1fe2d96b6a43b88f87a7ac7be625fa0
祝旭 <1150559257@qq.com>
2022-10-14 22:50:55 +0800

    更新学习

---
+++ Code/22-10-14/22-10-14/test.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+
+//int main()
+//{
+//	printf("file:%s line:%d\n", __FILE__, __LINE__);
+//	return 0;
+//}
+
+//#define PRINT(FORMAT, VALUE) printf("the value of "#VALUE" is "FORMAT"\n",VALUE);
+//
+//int main()
+//{
+//	int i = 10;
+//	PRINT("%d",i);
+//	PRINT("%d", i+3);
+//	return 0;
+//}
