4d82151fb155864bd2f310892fd775258907bb68
祝旭 <1150559257@qq.com>
2022-10-25 22:08:41 +0800

    更新学习

---
+++ Code/22-9-19/22-9-19/test.c
@@ -0,0 +1,50 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+//
+//void GetMemory(char *p)
+//{
+//	p = (char *)malloc(100);
+//}
+//
+//int main()
+//{
+//	char *str = NULL;
+//	GetMemory(str);
+//	strcpy(str, "hello world");
+//	printf(str);
+//	return 0;
+//}
+
+//#include <string.h>
+//int main()
+//{
+//	int arr1[5] = { 1, 2, 3, 4, 5 };
+//	int arr2[5] = { 1, 2, 4, 5, 6 };
+//	printf("%d\n", memcmp(arr1, arr2, 9));
+//	return 0;
+//}
+
+
+
+//char* GetMemory()
+//{
+//	char *p = "dfgdfgdf";
+//	return p;
+//}
+//
+//int main()
+//{
+//	char *str = NULL;
+//	str=GetMemory();
+//	printf(str);
+//	return 0;
+//}
+
+int main()
+{
+	int arr[] = { 1, 2, 3, 4, 5, 6 };
+	return 0;
+}
