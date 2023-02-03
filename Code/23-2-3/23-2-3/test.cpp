1524cf02e8acd3d2255e13f685066552008ab023
祝旭 <1150559257@qq.com>
2023-02-03 21:36:44 +0800

    更新学习

---
+++ Code/23-2-3/23-2-3/test.cpp
@@ -0,0 +1,74 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <iostream>
+using namespace std;
+
+
+//struct listnode
+//{
+//	int val;
+//	listnode* next;
+//};
+
+
+//class Stu
+//{
+//public:
+//	void SetstuInfo(char* name,int age,char* tele)
+//	{
+//		strcpy(_name, name);
+//		_age = age;
+//		strcpy(_tele, tele);
+//	}
+//
+//	void InfoPrint()
+//	{
+//		cout << _name << " " << _age << " " << _tele << endl;
+//	}
+//private:
+//	char _name[10];
+//	int _age;
+//	char _tele[11];
+//};
+//
+//
+//int main()
+//{
+//	Stu s1;
+//	s1.SetstuInfo("zhuxu", 23, "17342983498");
+//	s1.InfoPrint();
+//	return 0;
+//}
+
+
+//class Stu
+//{
+//public:
+//	void SetstuInfo(char* name,int age,char* tele)
+//	{
+//		strcpy(_name, name);
+//		_age = age;
+//		strcpy(_tele, tele);
+//	}
+//
+//	void InfoPrint();
+//
+//private:
+//	char _name[10];
+//	int _age;
+//	char _tele[11];
+//};
+//
+//
+//void Stu::InfoPrint()
+//{
+//	cout << _name << " " << _age << " " << _tele << endl;
+//}
+//
+//int main()
+//{
+//	Stu s1;//类的实例化
+//	s1.SetstuInfo("zhuxu", 23, "17342983498");
+//	s1.InfoPrint();
+//	return 0;
+//}
