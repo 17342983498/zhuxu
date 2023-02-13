06d217314743826508c6ae05c97b8ce416f6de56
祝旭 <1150559257@qq.com>
2023-02-13 21:34:45 +0800

    更新学习

---
+++ Code/23-2-13/23-2-13/test.cpp
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <iostream>
+using namespace std;
+
+
+//int Add(int a, int b)
+//{
+//	return a + b;
+//}
+//
+//double Add(double a, double b)
+//{
+//	return a + b;
+//}
+//
+//
+//int main()
+//{
+//	Add(1, 2);
+//	Add(1.2, 2.3);
+//	return 0;
+//}
+
+
+inline int Add(int a, int b)
+{
+	return a + b;
+}
+
+
+int main()
+{
+	Add(1, 2);
+	return 0;
+}
